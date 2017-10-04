a59b909838163e438936622352c61a578dd60239
Ian Foose <ianfoose@me.com>
2017-10-03 22:16:57 -0400

    Initial Commit

---
+++ Relaxful/Relaxful.h
@@ -0,0 +1,19 @@
+//
+//  Relaxful.h
+//  Relaxful
+//
+//  Created by Ian Foose on 10/3/17.
+//  Copyright © 2017 Foose Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Relaxful.
+FOUNDATION_EXPORT double RelaxfulVersionNumber;
+
+//! Project version string for Relaxful.
+FOUNDATION_EXPORT const unsigned char RelaxfulVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relaxful/PublicHeader.h>
+
+
